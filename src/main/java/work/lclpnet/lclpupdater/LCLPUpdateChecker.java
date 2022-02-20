a5abaca7cdc7e9b976221cdc27376b3fc3eabdf7
LCLP <lukas@lclpnet.work>
2022-02-20 15:49:12 +0100

    compilable

--- src/main/java/work/lclpnet/lclpupdater/LCLPUpdateChecker.java
+++ src/main/java/work/lclpnet/lclpupdater/LCLPUpdateChecker.java
@@ -1,41 +1,17 @@
 package work.lclpnet.lclpupdater;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.fabricmc.api.ClientModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import work.lclpnet.lclpupdater.util.MainScreenChecker;
 import work.lclpnet.lclpupdater.util.UpdateChecker;
 
-@Mod(LCLPUpdateChecker.MOD_ID)
-public class LCLPUpdateChecker {
+public class LCLPUpdateChecker implements ClientModInitializer {
 
     public static final String MOD_ID = "lclpupdater";
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public LCLPUpdateChecker() {
-        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modBus.addListener(this::setup);
-        modBus.addListener(this::onIMCProcess);
-
-        IEventBus bus = MinecraftForge.EVENT_BUS;
-        bus.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-        LOGGER.info("LCLPUpdateChecker initializing...");
-
+    @Override
+    public void onInitializeClient() {
         UpdateChecker.checkForUpdates();
-
-        LOGGER.info("LCLPUpdateChecker initialized.");
     }
-
-    public void onIMCProcess(InterModProcessEvent e) {
-        e.getIMCStream().forEach(MainScreenChecker::tryAccept);
-    }
-
 }
--- src/main/java/work/lclpnet/lclpupdater/util/UpdateChecker.java
+++ src/main/java/work/lclpnet/lclpupdater/util/UpdateChecker.java
@@ -1,15 +1,21 @@
 package work.lclpnet.lclpupdater.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import work.lclpnet.lclpupdater.event.UpdateCheckCompleteEvent;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class UpdateChecker {
 
-    private static volatile boolean updateRequired = false;
+    private static final Logger logger = LogManager.getLogger();
+    private static final AtomicBoolean updateRequired = new AtomicBoolean(false);
 
     public static boolean needsUpdate() {
-        return updateRequired;
+        return updateRequired.get();
     }
 
     public static void checkForUpdates() {
@@ -17,21 +23,21 @@ public class UpdateChecker {
     }
 
     private static void check() {
-        System.out.println("Checking for updates...");
+        logger.info("Checking for updates...");
 
         Helper.getLCLPLauncherExecutable()
                 .thenAcceptAsync(UpdateChecker::checkForUpdates)
                 .exceptionally(ex -> {
-                    ex.printStackTrace();
+                    logger.error("Could not check for updates", ex);
                     return null;
                 });
     }
 
-    private static void checkForUpdates(String exe) {
-        if (exe == null) throw new IllegalStateException("LCLPLauncher executable could not be found.");
+    private static void checkForUpdates(String program) {
+        if (program == null) throw new IllegalStateException("LCLPLauncher executable could not be found.");
 
         try {
-            Process process = new ProcessBuilder(exe, "check-for-app-update", "ls5")
+            Process process = new ProcessBuilder(program, "check-for-app-update", "ls5")
                     .redirectErrorStream(true)
                     .start();
 
@@ -39,16 +45,18 @@ public class UpdateChecker {
                 String line;
                 while ((line = in.readLine()) != null) {
                     if ("[update-check]: Result -> NEEDS_UPDATE".equals(line)) {
-                        updateRequired = true;
-                        System.out.println("An update is available.");
+                        updateRequired.set(true);
                         break;
                     }
                 }
                 process.waitFor();
-                if (!updateRequired) System.out.println("No update available.");
+
+                final boolean updateAvailable = updateRequired.get();
+                logger.info(updateAvailable ? "An update is available." : "Already up-to-date.");
+                UpdateCheckCompleteEvent.EVENT.invoker().onUpdateCheckComplete(updateAvailable);
             }
         } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            throw new IllegalStateException(e);
         }
     }
 }
